da793b2c85f92659b3c33b0e3cd4ffcad1b7bbf5
satishgamer89 <152015658+satishgamer89@users.noreply.github.com>
2024-01-30 20:49:07 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -20,5 +20,4 @@ sr.reveal('.home-text h1',{delay:680, origin:'left'})
 sr.reveal('.home-text p',{delay:750, origin:'right'})
 sr.reveal('.main-btn',{delay:860, origin:'left'})
 
-sr.reveal('.social',{delay:950, origin:'bottom'})
 sr.reveal('.home-img',{delay:1000, origin:'right'})
